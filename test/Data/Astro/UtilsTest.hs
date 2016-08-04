786112952b8c1fe49bb22f07b0e5bb99cf651788
Alexander Ignatyev <alexander.ignatyev@yahoo.com>
2016-08-04 14:52:11 +0300

    added Utils unit tests

--- test/Data/Astro/UtilsTest.hs
+++ test/Data/Astro/UtilsTest.hs
@@ -17,17 +17,22 @@ import Data.Fixed(Pico(..))
 import Data.Astro.Utils
 
 tests = [testGroup "fromFixed" [
-            testProperty "fromFixed Property" prop_fromFixed
+            testProperty "property" prop_fromFixed
+            , testCase "5.111123" $ assertApproxEqual "" 0.000000001 5.111123 $ fromFixed (toPico 5.111123)
+            , testCase "-999.9999" $ assertApproxEqual "" 0.000000001 (-999.9999) $ fromFixed (toPico (-999.9999))
             ]
         , testGroup "fraction" [
-            testProperty "fraction Property" prop_fraction
+            testProperty "property" prop_fraction
             ]
         , testGroup "trunc" [
-            testProperty "trunc Property" prop_trunc
+            testProperty "property" prop_trunc
             , testCase "5.562" $ 5.0 @=? trunc 5.562
             , testCase "-7.93" $ (-7.0) @=? trunc (-7.93)
             ]
         ]
+
+toPico :: Real a => a -> Pico
+toPico = realToFrac
 
 prop_fromFixed d =
   abs((fromFixed ((realToFrac d)::Pico))-d) < 0.0000001
