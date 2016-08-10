903d56e0b2b38aaaf895fe129a765264f5c77278
Alexander Ignatyev <alexander.ignatyev@yahoo.com>
2016-08-10 15:59:24 +0300

    refract supports angles < 15

--- src/Data/Astro/Effects.hs
+++ src/Data/Astro/Effects.hs
@@ -19,5 +19,17 @@ import Data.Astro.Types (DecimalDegrees(..), toRadians)
 -- It takes the observed altitude (of Horizon Coordinates), temperature in degrees centigrade and barometric pressure in millibars.
 refract :: DecimalDegrees -> Double -> Double -> DecimalDegrees
 refract altitude temperature pressure =
+  let f = if altitude > (DD 15) then refractHighAltitude else refractLowAltitude
+  in f altitude temperature pressure
+
+
+refractHighAltitude :: DecimalDegrees -> Double -> Double -> DecimalDegrees
+refractHighAltitude altitude temperature pressure =
   let z = toRadians $ 90 - altitude  -- zenith angle
   in DD $ 0.00452*pressure*(tan z) /(273+temperature) 
+
+
+refractLowAltitude :: DecimalDegrees -> Double -> Double -> DecimalDegrees
+refractLowAltitude altitude temperature pressure =
+  let a = toRadians altitude
+  in DD $ pressure*(0.1594+0.0196*a+0.00002*a*a)/((273+temperature)*(1+0.505*a+0.0845*a*a))
--- test/Data/Astro/EffectsTest.hs
+++ test/Data/Astro/EffectsTest.hs
@@ -18,9 +18,21 @@ import Data.Astro.Types (DecimalDegrees(..))
 import Data.Astro.Effects
 
 tests = [testGroup "refraction" [
-            testDecimalDegrees ""
-              0.000001
-              (DD 0.045403)
-              (refract (DD 19.334345) 13 1008)
+            testDecimalDegrees "19.33"
+                0.000001
+                (DD 0.045403)
+                (refract (DD 19.334345) 13 1008)
+            , testDecimalDegrees "horizon"
+                0.000001
+                (DD 0.566569)
+                (refract (DD 0) 12 1013)
+            , testDecimalDegrees "azimuth"
+                0.000001
+                (DD 0)
+                (refract (DD 90) 12 1012)
+            , testDecimalDegrees "azimuth"
+                0.000001
+                (DD 0.007905)
+                (refract (DD 63.5) 15.5 1012)
             ]
         ]
