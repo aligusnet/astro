e0ba4016f04ee1e523c63b28efeceb564e6b74dc
Alexander Ignatyev <alexander.ignatyev@yahoo.com>
2016-08-12 16:58:08 +0300

    added lstToLCT function of Time module

--- src/Data/Astro/Time.hs
+++ src/Data/Astro/Time.hs
@@ -10,16 +10,17 @@ Root Time module
 module Data.Astro.Time
 (
   lctToLST
+  , lstToLCT
 )
 
 where
 
 import Data.Astro.Types (DecimalDegrees)
-import Data.Astro.Time.JulianDate (JulianDate, lctToUT)
-import Data.Astro.Time.Sidereal (utToGST, gstToLST)
+import Data.Astro.Time.JulianDate (JulianDate, utToLCT, lctToUT)
+import Data.Astro.Time.Sidereal (utToGST, gstToUT, gstToLST, lstToGST)
 
 
--- | Local Civil Time to Local Sidereal Time.Ty
+-- | Local Civil Time to Local Sidereal Time.
 -- It takes longitude in decimal degrees, time zone and local civil time
 lctToLST :: DecimalDegrees -> Double -> JulianDate -> JulianDate
 lctToLST longitude timeZone lct =
@@ -27,3 +28,14 @@ lctToLST longitude timeZone lct =
       gst = utToGST ut
       lst = gstToLST longitude gst
   in lst
+
+
+-- | Local Sidereal Time to Local Civil Time.
+-- It takes longitude in decimal degrees, time zone and local sidereal time
+lstToLCT :: DecimalDegrees -> Double -> JulianDate -> JulianDate
+lstToLCT longitude timeZone lst =
+  let gst = lstToGST longitude lst
+      ut = gstToUT gst
+      lct = utToLCT timeZone ut
+  in lct
+
