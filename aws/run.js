ea5338c22e5d05478bb78080f4e819904d1add4e
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2017-06-11 09:55:09 +0100

    small tweaks in run.js

--- aws/run.js
+++ aws/run.js
@@ -1,8 +1,11 @@
-const spawn = require('child_process').spawn;
-var exec = require('child_process').exec;
+const exec = require('child_process').exec;
 
 exports.handle = function(event, context) {
+    console.log('starting app with event', event);
     exec('./main --json="'+ JSON.stringify(event) + '"', function(error, stdout) {
+        console.log('app exited');
+        console.log('error:', error);
+        console.log('stdout:', stdout);
         context.done(error, JSON.parse(stdout));
     });
 }
