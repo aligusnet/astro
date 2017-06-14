bfab59052b3f8ed0b11d04053e75d9341ec1393c
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2017-06-14 22:00:13 +0100

    minor fix in run.js

--- aws/run.js
+++ aws/run.js
@@ -2,7 +2,7 @@ const { execFile } = require('child_process');
 
 exports.handle = function(event, context) {
     console.log('starting app with event', event);
-    execFile('./main', ['--json', '"'+ JSON.stringify(event) + '"'], function(error, stdout) {
+    execFile('./main', ['--json', event], function(error, stdout) {
         console.log('app exited');
         console.log('error:', error);
         console.log('stdout:', stdout);
