c1a22b600265731411677c259d7189f1936df3c5
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2017-06-17 16:34:44 +0100

    added new stars

--- src/Data/Astro/Star.hs
+++ src/Data/Astro/Star.hs
@@ -23,6 +23,12 @@ import Data.Astro.Types (fromDMS, fromHMS)
 data Star = Polaris
             | AlphaCrucis
             | Sirius
+            | Betelgeuse
+            | Rigel
+            | Vega
+            | Antares
+            | Canopus
+            | Pleiades
               deriving (Show, Eq)
 
 
@@ -31,4 +37,10 @@ starCoordinates :: Star -> EquatorialCoordinates1
 starCoordinates Polaris = EC1 (fromDMS 89 15 51) (fromHMS 2 31 48.7)
 starCoordinates AlphaCrucis = EC1 (-(fromDMS 63 5 56.73)) (fromHMS 12 26 35.9)
 starCoordinates Sirius = EC1 (-(fromDMS 16 42 58.02)) (fromHMS 6 45 8.92)
+starCoordinates Betelgeuse = EC1 (fromDMS 07 24 25.4304) (fromHMS 5 55 10.30536)
+starCoordinates Rigel = EC1 (-(fromDMS 8 12 05.8981)) (fromHMS 5 14 32.27210)
+starCoordinates Vega = EC1 (fromDMS 38 47 01.2802) (fromHMS 18 36 56.33635)
+starCoordinates Antares = EC1 (-(fromDMS 26 25 55.2094)) (fromHMS 16 29 24.45970)
+starCoordinates Canopus = EC1 (-(fromDMS 52 41 44.3810)) (fromHMS 6 23 57.10988)
+starCoordinates Pleiades = EC1 (fromDMS 24 7 00) (fromHMS 3 47 24)
 
