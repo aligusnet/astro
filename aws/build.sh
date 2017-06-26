5b63de33f84498970b588af1470e4d35276138a9
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2017-06-26 19:48:51 +0100

    added unit tests files to cabal file, moved astro-app into separate folder to reduce number of dependencies of the package

--- aws/build.sh
+++ aws/build.sh
@@ -1,5 +1,6 @@
+cd app
 stack build --allow-different-user
-dd if=`stack path --local-install-root`/bin/astro-app > aws/main
-cd aws
+dd if=`stack path --local-install-root`/bin/astro-app > ../aws/main
+cd ../aws
 strip main
 chmod +x main
